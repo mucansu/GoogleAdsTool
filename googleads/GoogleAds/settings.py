c77f5525f44e00c6e88c97c5c727ae97c28e307a
Mustafa <mucansu@gmail.com>
2024-01-27 11:05:26 +0000

    websocket and files path changed

--- googleads/GoogleAds/settings.py
+++ googleads/GoogleAds/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$k^y!!-+fxxuo1(+my=!7x_n$8h!1#r+fflf5gls2xyx7aftlr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = ['127.0.0.1', 'mustafa.mintyazilim.com']
+#mustafa.mintyazilim.com
 
 # Application definition
 
